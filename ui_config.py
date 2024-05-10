cb5c7bee503a76f2c837667db07c81633f61e7d7
Tilt <76159417+ashisharyan675@users.noreply.github.com>
2024-05-10 22:52:26 +0530

    Add files via upload

---
+++ ui_config.py
@@ -0,0 +1,6 @@
+drivers_config = {
+    "login_url": "http://20.197.13.140:5173/",
+    "Dashboard_url": "http://20.197.13.140:5173/dashboard",
+    "Admin_employee_url": "http://20.197.13.140:5173/admin/manageemployees",
+    "Admin_lob_url": "http://20.197.13.140:5173/admin/LOB"
+}
