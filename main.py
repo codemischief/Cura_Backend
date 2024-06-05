e4ef5f9e3a106e335b3b48b549917871548100a8
Rudra Sen Mallik <theruderaw678@gmail.com>
2024-06-05 19:19:21 +0530

    Fixed isdeleted in getusersadmin

--- main.py
+++ main.py
@@ -2034,7 +2034,7 @@ async def get_users_admin(payload: dict, conn : psycopg2.extensions.connection =
         role_access_status = check_role_access(conn,payload)
         if role_access_status==1:
             with conn[0].cursor() as cursor:
-                query = "SELECT firstname,lastname,id,username from usertable order by firstname where isdeleted=false"
+                query = "SELECT firstname,lastname,id,username from usertable where isdeleted=false order by firstname"
                 msg = logMessage(cursor,query)
                 logging.info(msg)
                 arr = []
